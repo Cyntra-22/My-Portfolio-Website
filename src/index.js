6423bd02c42b57010e479699d774ddf8f5e101d1
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2023-08-07 13:13:00 +0700

    Updated the index.js for share on social media, html and css to modify

--- src/index.js
+++ src/index.js
@@ -6,3 +6,45 @@ menuBtn.addEventListener("click", () =>{
     sideBar.classList.toggle("active-nav")
     container.classList.toggle("active-content");
 })
+
+ function shareOnFacebook() {
+   const urlToShare = "https://fantastic-praline-34322e.netlify.app/";
+   const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+     urlToShare
+   )}`;
+   window.open(facebookUrl, "_blank", "width=600,height=400");
+ }
+
+ function shareOnLinkedIn() {
+   const urlToShare = "https://fantastic-praline-34322e.netlify.app/";
+   const linkedInUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+     urlToShare
+   )}`;
+   window.open(linkedInUrl, "_blank", "width=600,height=400");
+ }
+
+ function shareOnTwitter() {
+   const urlToShare = "https://fantastic-praline-34322e.netlify.app/";
+   const textToShare = "Text to be shared on Twitter";
+   const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+     urlToShare
+   )}&text=${encodeURIComponent(textToShare)}`;
+   window.open(twitterUrl, "_blank", "width=600,height=400");
+ }
+
+ function shareOnFacebookMessenger() {
+   const urlToShare = "https://fantastic-praline-34322e.netlify.app/";
+   const facebookMessengerUrl = `fb-messenger://share/?link=${encodeURIComponent(
+     urlToShare
+   )}`;
+   window.open(facebookMessengerUrl, "_blank", "width=600,height=400");
+ }
+
+  function shareOnWhatsapp() {
+    const urlToShare = "https://fantastic-praline-34322e.netlify.app/";
+    const whatsappUrl = `https://api.whatsapp.com/send?text=Your%20encoded%20text%20or%20URL%20to%20share=${encodeURIComponent(
+      urlToShare
+    )}`;
+    window.open(whatsappUrl, "_blank", "width=600,height=400");
+  }
+
